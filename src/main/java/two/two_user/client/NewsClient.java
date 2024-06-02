83bda4cde08825187145f4ee1e6de90e5ea7027c
haden <80521474+thdefn@users.noreply.github.com>
2024-06-02 19:58:59 +0900

    feat: add scrap count in profile

--- src/main/java/two/two_user/client/NewsClient.java
+++ src/main/java/two/two_user/client/NewsClient.java
@@ -3,12 +3,13 @@ package two.two_user.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "newsClient", url = "${feign.news}")
 public interface NewsClient {
-    @PostMapping(value = "/news/bookmark")
+    @GetMapping(value = "/news/bookmark")
     ResponseEntity<Long> getBookMark(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String token);
 }
