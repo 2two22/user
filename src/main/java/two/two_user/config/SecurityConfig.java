a25d58cbbabc7147ba0342494708a94e24737851
haden <80521474+thdefn@users.noreply.github.com>
2024-06-02 22:22:26 +0900

    chore: set allowed origin

--- src/main/java/two/two_user/config/SecurityConfig.java
+++ src/main/java/two/two_user/config/SecurityConfig.java
@@ -40,9 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://127.0.0.1:5173",
-                "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowCredentials(true);
